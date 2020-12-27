48ccd070bc49a18e84295e038c6aac1a2055251c
Simon Nielsen <simon.c.nielsen@gmail.com>
2020-12-27 14:34:30 +0100

    Add first api call

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 
 export default function HomePage() {
+  const [apiOK, setApiOK] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    fetch('https://simon-cookbook-backend.herokuapp.com/')
+      .then((res) => res.text())
+      .then((text) => {
+        if (text) {
+          setApiOK(true);
+        }
+      });
+  });
+
   return (
     <div>
       <h1>Cookbook frontend</h1>
+      <h2>API {apiOK ? 'OK' : 'not OK'}</h2>
     </div>
   );
 }
